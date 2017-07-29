25b726be3958e4a1702b0f901e34121a6dc45756
RobertoGalois <victor.vague@live.fr>
2017-07-29 16:46:16 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+ #define MAIN_H
+ void sort_array(int p_array[], signed int p_size, unsigned short p_way);
+ void switch_values(int *ptr1, int *ptr2);
+ void list_array(int p_array[], unsigned int p_size);
+#endif
